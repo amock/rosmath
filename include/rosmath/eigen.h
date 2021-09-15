b2662715146aa81a00bd31a9543fa1d7ed5a462e
Alexander Mock <amock@uos.de>
2021-09-15 16:40:11 +0200

    fix

---
+++ include/rosmath/eigen.h
@@ -0,0 +1,7 @@
+#ifndef ROSMATH_EIGEN_HPP
+#define ROSMATH_EIGEN_HPP
+
+#include "eigen/conversions.h"
+#include "eigen/stats.h"
+
+#endif // ROSMATH_EIGEN_HPP
--- include/rosmath/usingoperators.h
+++ include/rosmath/usingoperators.h
@@ -1,8 +1,6 @@
 #ifndef ROSMATH_USING_OPERATORS_H
 #define ROSMATH_USING_OPERATORS_H
 
-#include "rosmath.h"
-
 using rosmath::operator<<=;
 using rosmath::operator+;
 using rosmath::operator-;
